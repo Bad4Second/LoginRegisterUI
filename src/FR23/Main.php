32fae729f0dacca31fa86e953e21ce89ab722347
!fr? <130619876+Bad4Second@users.noreply.github.com>
2023-04-13 09:32:15 +0800

    Add files via upload

---
+++ src/FR23/Main.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace FR23;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use jojoe77777\FormAPI\{SimpleForm, CustomForm};
+use FR23\EventListener;
+
+class Main extends PluginBase {
+
+    private $config;
+    private $players;
+
+    public function onEnable(): void {
+        $this->saveDefaultConfig();
+        $this->config = $this->getConfig();
+        $this->players = new Config($this->getDataFolder() . "players.yml", Config::YAML);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    }
+
+    public function onPlayerJoin(Player $player): void {
+        if (!$this->players->exists($player->getName())) {
+            $this->showRegisterUI($player);
+        } else {
+            $this->showLoginUI($player);
+        }
+    }
+
+    private function showRegisterUI(Player $player): void {
+        $form = new CustomForm(function(Player $player, $data): void {
+            if ($data === null) {
+                $player->kick(TextFormat::RED . "Registration cancelled", false);
+                return;
+            }
+            $password = $data[1];
+            $this->players->set($player->getName(), $password);
+            $this->players->save();
+            $player->sendMessage(TextFormat::GREEN . "You have successfully registered!");
+        });
+        $form->setTitle("§l§6Register");
+        $form->addInput("Enter a password", "Example: pw123");
+        $form->addInput("Confirm password", "Example: pw123");
+        $player->sendForm($form);
+    }
+
+    private function showLoginUI(Player $player): void {
+        $form = new CustomForm(function(Player $player, $data): void {
+            if ($data === null) {
+                $player->kick(TextFormat::RED . "Login cancelled", false);
+                return;
+            }
+            $name = $player->getName();
+            $password = $data[0];
+            $savedPassword = $this->players->get($name);
+            if ($password === $savedPassword) {
+                $player->sendMessage(TextFormat::GREEN . "You have successfully logged in!");
+            } else {
+                $player->kick(TextFormat::RED . "INVALID PASSWORD\n\n§eIf you forget your Password, please contact the Admin/Owner", false);
+            }
+        });
+        $form->setTitle("§l§6Login");
+        $form->addInput("Enter your password", "Example: pw123");
+        $player->sendForm($form);
+    }
+}
