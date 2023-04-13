aecf2aa6f3b9fecca8628def0b34d581b7e83361
!fr? <130619876+Bad4Second@users.noreply.github.com>
2023-04-13 09:33:01 +0800

    Add files via upload

---
+++ src/FR23/EventListener.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FR23;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class EventListener implements Listener {
+
+    private $main;
+
+    public function __construct(Main $main) {
+        $this->main = $main;
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+        $this->main->onPlayerJoin($player);
+    }
+}
